a6721150dbeae870b71b7fcfd58184e68f341dd2
Victor Baleeiro <victor.baleeiro@going2.com.br>
2019-01-04 19:01:13 -0200

    código próprio para command pattern

--- Behavioral.playground/Pages/Iterator.xcplaygroundpage/Contents.swift
+++ Behavioral.playground/Pages/Iterator.xcplaygroundpage/Contents.swift
@@ -20,7 +20,7 @@ public struct LibraryIterator : IteratorProtocol {
     }
     
     mutating public func next() -> Book? {
-        defer{ currentIndex = currentIndex + 1}
+        defer { currentIndex = currentIndex + 1 }
         return currentIndex >= lib.catalog.count ? nil : lib.catalog[currentIndex]
     }
 }
@@ -32,10 +32,50 @@ extension Library : Sequence {
 }
 
 
-let catalog = [Book(name:"Bla"),Book(name:"Blub"),Book(name:"Foo")]
+let catalog = [Book(name:"Bla"), Book(name:"Blub"), Book(name:"Foo")]
 let library = Library(catalog: catalog)
 
 for book in library {
     print(book.name)
 }
+
+
+// Victor's code
+public struct Model {
+    var name: String
+}
+
+public struct IPhoneCatalog {
+    var models = [Model]()
+}
+
+public struct IPhoneCatalogIterator: IteratorProtocol {
+    
+    public var iPhoneCatalog: IPhoneCatalog
+    private var currentIndex: Int = 0
+
+    public init(iPhoneCatalog: IPhoneCatalog) {
+        self.iPhoneCatalog = iPhoneCatalog
+    }
+
+    mutating public func next() -> Model? {
+        defer { currentIndex = currentIndex + 1 }
+        return currentIndex >= iPhoneCatalog.models.count ? nil : iPhoneCatalog.models[currentIndex]
+    }
+}
+
+extension IPhoneCatalog : Sequence {
+    public func makeIterator() -> IPhoneCatalogIterator {
+        return IPhoneCatalogIterator(iPhoneCatalog: self)
+    }
+}
+
+let models = [Model(name:"iPhone 5S"), Model(name:"iPhone 8"), Model(name:"iPhone X")]
+let iphoneCatalog = IPhoneCatalog(models: models)
+
+for model in iphoneCatalog {
+    print(model.name)
+}
 //: [Next](@next)
+
+
