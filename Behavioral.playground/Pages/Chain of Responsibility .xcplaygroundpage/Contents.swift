50328dbaa5391aa42171f9e81bf9652a18e496b0
Victor Baleeiro <victor.baleeiro@going2.com.br>
2019-01-04 17:28:17 -0200

    código próprio para chain of responsability

--- Behavioral.playground/Pages/Chain of Responsibility .xcplaygroundpage/Contents.swift
+++ Behavioral.playground/Pages/Chain of Responsibility .xcplaygroundpage/Contents.swift
@@ -52,6 +52,118 @@ var jobs = [Job(minSkill: .Junior, name: "Windshield Wiper"),
 for job in jobs{
     shop.performJob(job: job)
 }
+
+
+// Victor code
+class Despesa {
+    
+    private var _total = Int()
+    var total: Int {
+        get {
+            return _total
+        }
+        
+        set {
+            _total = newValue
+        }
+    }
+    
+    init(total: Int) {
+        self.total = total
+    }
+}
+
+protocol Chain {
+    var proximoNivelGerenciamento: Chain { get set }
+    func deveAprovarDespesa(despesa: Despesa)
+}
+
+class CEO: Chain {
+    private var _proximoNivelGerenciamento: Chain?
+    var proximoNivelGerenciamento: Chain {
+        get {
+            return _proximoNivelGerenciamento!
+        }
+        
+        set {
+            _proximoNivelGerenciamento = newValue
+        }
+    }
+    
+    func deveAprovarDespesa(despesa: Despesa) {
+        if despesa.total > 1001 && despesa.total < 10000 {
+            print("O CEO pode aprovar esta despesa")
+        } else {
+            print("Esta despesa é muito alta e não será aprovada")
+        }
+    }
+}
+
+class Boss: Chain {
+    private var _proximoNivelGerenciamento: Chain?
+    var proximoNivelGerenciamento: Chain {
+        get {
+            return _proximoNivelGerenciamento!
+        }
+        
+        set {
+            _proximoNivelGerenciamento = newValue
+        }
+    }
+    
+    func deveAprovarDespesa(despesa: Despesa) {
+        if despesa.total > 101 && despesa.total < 1000 {
+            print("Seu boss pode aprovar esta despesa")
+        } else {
+            if _proximoNivelGerenciamento != nil {
+                proximoNivelGerenciamento.deveAprovarDespesa(despesa: despesa)
+            }
+        }
+    }
+}
+
+class Funcionario: Chain {
+    private var _proximoNivelGerenciamento: Chain?
+    var proximoNivelGerenciamento: Chain {
+        get {
+            return _proximoNivelGerenciamento!
+        }
+        
+        set {
+            _proximoNivelGerenciamento = newValue
+        }
+    }
+    
+    func deveAprovarDespesa(despesa: Despesa) {
+        if despesa.total > 0 && despesa.total < 100 {
+            print("Um funcionário pode aprovar esta despesa")
+        } else {
+            if _proximoNivelGerenciamento != nil {
+                proximoNivelGerenciamento.deveAprovarDespesa(despesa: despesa)
+            }
+        }
+    }
+}
+
+let funcionario = Funcionario()
+let boss = Boss()
+let ceo = CEO()
+
+funcionario.proximoNivelGerenciamento = boss
+boss.proximoNivelGerenciamento = ceo
+
+let despesa = Despesa(total: 5)
+funcionario.deveAprovarDespesa(despesa: despesa)
+
+despesa.total = 500
+funcionario.deveAprovarDespesa(despesa: despesa)
+
+despesa.total = 5000
+funcionario.deveAprovarDespesa(despesa: despesa)
+
+despesa.total = 50000
+funcionario.deveAprovarDespesa(despesa: despesa)
+
 //: [Next](@next)
 
 
